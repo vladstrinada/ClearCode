90ec0a48672d1d03d2aac7cbb300eaf7658762fd
vladstrinada <vladstr@icloud.com>
2018-02-04 21:59:24 +0100

    first commit

---
+++ CCFrontTask2.js
@@ -0,0 +1,11 @@
+exports.generateRunicWords = length => {
+
+    
+    const runes =[
+    {name:'El',power:28,cantBeLinkedWith:'Ort'},{name:'Eld',power:33,cantBeLinkedWith:'Sur'},{name:'Tir',power:9,cantBeLinkedWith:'Eth'},{name:'Nef',power:7,cantBeLinkedWith:'Ist'},{name:'Eth',power:31,cantBeLinkedWith:'Tir'},{name:'Ith',power:22,cantBeLinkedWith:'Pul'},{name:'Tal',power:8,cantBeLinkedWith:'Io'},{name:'Ral',power:25,cantBeLinkedWith:'Um'},{name:'Ort',power:18,cantBeLinkedWith:'El'},{name:'Thul',power:13,cantBeLinkedWith:'Sol'},{name:'Amn',power:6,cantBeLinkedWith:'Fal'},{name:'Sol',power:10,cantBeLinkedWith:'Thul'},{name:'Shael',power:17,cantBeLinkedWith:'Lem'},{name:'Dol',power:11,cantBeLinkedWith:'Hel'},{name:'Hel',power:12,cantBeLinkedWith:'Dol'},{name:'Io',power:20,cantBeLinkedWith:'Tal'},{name:'Lum',power:32,cantBeLinkedWith:'Gul'},{name:'Ko',power:27,cantBeLinkedWith:'Mal'},{name:'Fal',power:14,cantBeLinkedWith:'Amn'},{name:'Lem',power:26,cantBeLinkedWith:'Shall'},{name:'Pul',power:15,cantBeLinkedWith:'Ith'},{name:'Um',power:16,cantBeLinkedWith:'Ral'},{name:'Mal',power:21,cantBeLinkedWith:'Ko'},{name:'Ist',power:4,cantBeLinkedWith:'Nef'},{name:'Gul',power:23,cantBeLinkedWith:'Lum'},{name:'Vex',power:24,cantBeLinkedWith:'Ohm'},{name:'Ohm',power:1,cantBeLinkedWith:'Vex'},{name:'Lo',power:2,cantBeLinkedWith:'Cham'},{name:'Sur',power:30,cantBeLinkedWith:'Eld'},{name:'Ber',power:3,cantBeLinkedWith:''},{name:'Jah',power:3,cantBeLinkedWith:'Zod'},{name:'Cham',power:29,cantBeLinkedWith:'Lo'},{name:'Zod',power:19,cantBeLinkedWith:'Jah'}
+];
+    
+    
+    
+return runicWords;
+}
